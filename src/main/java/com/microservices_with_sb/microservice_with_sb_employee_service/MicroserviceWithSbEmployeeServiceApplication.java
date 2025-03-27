7554e8cc4116bfa24b17756dc63c2ba6f94e5c43
Rajesh <rajesh4.ramesh@gmail.com>
2025-03-27 13:17:40 +0530

    intial commit

---
+++ src/main/java/com/microservices_with_sb/microservice_with_sb_employee_service/MicroserviceWithSbEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices_with_sb.microservice_with_sb_employee_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceWithSbEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceWithSbEmployeeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices_with_sb/microservice_with_sb_employee_service/MicroserviceWithSbEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices_with_sb.microservice_with_sb_employee_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceWithSbEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
